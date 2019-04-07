f1a06adea79dff43a90dab60f342ba38c01887ed
aknimbal <48735789+aknimbal@users.noreply.github.com>
2019-04-06 22:52:20 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+class AnalysisResultsQueue:
+    account_name = "ccbcustorage"
+    account_key = "9Mg7JNNHVukysJgENg7XkWcf3egUgwNfQq25qZqftKkBR5IK1pzOiPSd9cb5cYS493aB5MwlHswVTMRJmD15cQ=="
+    queue_name = "analysis-process-akshay"
+
+class AnalysisResultsURL:
+    analysis_page: "https://pybuildproduct.azurewebsites.net/getpages"
+    analysis_detail_page: "https://pybuildproduct.azurewebsites.net/getpages"
+    
+
